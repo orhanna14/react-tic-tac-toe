bba9e4037167285aa63c57709aa70bfb0e0ccbab
Olivia McGoffin <olivia@revenantsoftware.com>
2020-08-04 16:15:12 -0700

    Bold the currently selected item in the move list.

--- src/index.js
+++ src/index.js
@@ -95,7 +95,8 @@ class Game extends React.Component {
         'Go to game start';
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button onClick={() => this.jumpTo(move)}
+          className={move === this.state.stepNumber ? "selected-move" : "button"}>{desc}</button>
         </li>
       );
     });
